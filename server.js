c8ebca099cb788cfe051df189bc44bad7395fb35
Ian Lamb <ianlamb32@gmail.com>
2015-07-11 01:10:01 -0400

    got the server working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http');
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
@@ -8,7 +8,6 @@ io.on('connection', function(socket) {
     socket.on('register', function(id) {
         socket.join('lobby');
     });
-    
     
     socket.on('disconnect', function() {
         console.log('socket disconnect');
@@ -19,9 +18,11 @@ io.on('connection', function(socket) {
     });
 });
 
-app.get('/', function(req, res){
-    res.sendfile('index.html');
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: __dirname + '/client' });
 });
+
+app.use(express.static(__dirname + '/client'));
 
 http.listen(1337, function() {
     console.log('Listening on port 1337');
