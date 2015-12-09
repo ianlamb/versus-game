c6443aefedd610e013a3555e705ecd47f2c871a1
Ian Lamb <ianlamb32@gmail.com>
2015-12-09 00:53:30 -0500

    game ui improvements

--- client/app/components/versus/versus.js
+++ client/app/components/versus/versus.js
@@ -2,35 +2,25 @@ angular.module('versus', [])
     .controller('VersusController', function($scope, $rootScope, $location) {
     'use strict';
     
-    if ($rootScope.socket) {
-        $rootScope.socket.on('match update', function(match) {
-            $rootScope.match = match;
-            $scope.$apply();
-        });
-        
-        $scope.gameResults = function() {
-            if (!$rootScope.match.inProgress) {
-                if ($rootScope.match.winner.name === $rootScope.player.name) {
-                    return 'You won!';
-                } else if ($rootScope.match.loser.name === $rootScope.player.name) {
-                    return 'You lost!';
-                }
-            }
-        };
-    } else {
+    if (!$rootScope.socket) {
         // mock match to test gameplay
         $rootScope.player = {
             id: 1,
             name: 'Jekyll',
-            health: 100
+            character: {
+                health: 100
+            }
         };
         $rootScope.match = {
             id: 'test',
             inProgress: true,
             p1: $rootScope.player,
             p2: {
+                id: 2,
                 name: 'Hyde',
-                health: 100
+                character: {
+                    health: 100
+                }
             }
         }
     }
@@ -74,7 +64,6 @@ angular.module('versus', [])
     }, false);
     
     var allowedToAttack = true;
-    var score = 0;
     var recoil = 0;
     var recoilRate = 40;
     var recoilRecoveryRate = recoilRate / 150;
@@ -83,25 +72,23 @@ angular.module('versus', [])
     stage.width = document.getElementById('vsCanvas').clientWidth;
     stage.height = document.getElementById('vsCanvas').clientHeight;
     
-    var pendulumBox = new createjs.Shape();
-    pendulumBox.width = stage.width;
-    pendulumBox.height = 100;
-    pendulumBox.y = stage.height - pendulumBox.height;
-    pendulumBox.graphics.beginFill('rgba(0,0,0,0.5)').drawRect(0, 0, pendulumBox.width, pendulumBox.height);
+    // pendulum
+    var pendulumContainer = new createjs.Container();
+    pendulumContainer.width = stage.width;
+    pendulumContainer.height = 100;
+    pendulumContainer.y = stage.height - pendulumContainer.height;
     
     var pendulum = new createjs.Shape();
     pendulum.width = 5;
-    pendulum.height = pendulumBox.height;
+    pendulum.height = pendulumContainer.height;
     pendulum.direction = 1;
     pendulum.baseSpeed = 5;
     pendulum.speed = pendulum.baseSpeed;
-    pendulum.y = stage.height - pendulum.height;
     pendulum.graphics.beginFill('rgba(0,180,255,1)').drawRect(0, 0, pendulum.width, pendulum.height);
     
     var pendulumShadow = new createjs.Shape();
     pendulumShadow.width = pendulum.width;
     pendulumShadow.height = pendulum.height;
-    pendulumShadow.y = pendulum.y;
     pendulumShadow.alpha = 0;
     pendulumShadow.graphics.beginFill('rgba(0,100,200,1)').drawRect(0, 0, pendulumShadow.width, pendulumShadow.height);
     
@@ -109,21 +96,83 @@ angular.module('versus', [])
     target.baseWidth = 100;
     target.minWidth = 10;
     target.width = target.baseWidth;
-    target.height = pendulumBox.height;
+    target.height = pendulumContainer.height;
     target.x = stage.width / 2 - target.width / 2;
-    target.y = stage.height - target.height;
     target.graphics.beginFill('rgba(225,0,0,0.4)').drawRect(0, 0, target.width, target.height);
     
-    var scoreText = new createjs.Text('Score: ' + score);
-    scoreText.x = 20;
-    scoreText.y = 20;
+    var pendulumBackground = new createjs.Shape();
+    pendulumBackground.graphics.beginFill('rgba(0,0,0,0.5)').drawRect(0, 0, stage.width, 100);
     
-    stage.addChild(pendulumBox);
-    stage.addChild(target);
-    stage.addChild(pendulum);
-    stage.addChild(pendulumShadow);
-    stage.addChild(scoreText);
+    pendulumContainer.addChild(pendulumBackground, target, pendulumShadow, pendulum);
+    
+    // player 1
+    var p1Name = new createjs.Text($rootScope.match.p1.name);
+    p1Name.color = '#fff';
+    
+    var p1HealthContainer = new createjs.Shape();
+    p1HealthContainer.width = 104;
+    p1HealthContainer.height = 24;
+    p1HealthContainer.y = 20;
+    p1HealthContainer.graphics.beginFill('rgba(0,0,0,0.5)').drawRect(0, 0, p1HealthContainer.width, p1HealthContainer.height);
+    
+    var p1Health = new createjs.Shape();
+    p1Health.height = 20;
+    p1Health.x = 2;
+    p1Health.y = 22;
+    p1Health.graphics.beginFill('rgba(0,180,255,1)').drawRect(0, 0, $rootScope.match.p1.character.health, p1Health.height);
+    
+    var p1Container = new createjs.Container();
+    p1Container.x = 20;
+    p1Container.y = 20;
+    p1Container.skewY = -3;
+    p1Container.skewX = 10;
+    p1Container.addChild(p1Name, p1HealthContainer, p1Health);
+    
+    // player 2
+    var p2Name = new createjs.Text($rootScope.match.p2.name);
+    p2Name.color = '#fff';
+    
+    var p2HealthContainer = new createjs.Shape();
+    p2HealthContainer.width = 104;
+    p2HealthContainer.height = 24;
+    p2HealthContainer.y = 20;
+    p2HealthContainer.graphics.beginFill('rgba(0,0,0,0.5)').drawRect(0, 0, p2HealthContainer.width, p2HealthContainer.height);
+    
+    var p2Health = new createjs.Shape();
+    p2Health.height = 20;
+    p2Health.x = 2;
+    p2Health.y = 22;
+    p2Health.graphics.beginFill('rgba(0,180,255,1)').drawRect(0, 0, $rootScope.match.p2.character.health, p2Health.height);
+    
+    var p2Container = new createjs.Container();
+    p2Container.width = 110;
+    p2Container.x = stage.width - p2Container.width - 20;
+    p2Container.y = 20;
+    p2Container.skewY = 3;
+    p2Container.skewX = -10;
+    p2Container.addChild(p2Name, p2HealthContainer, p2Health);
+    
+    stage.addChild(p1Container, p2Container, pendulumContainer);
     stage.update();
+    
+    if ($rootScope.socket) {
+        $rootScope.socket.on('match update', function(match) {
+            $rootScope.match = match;
+            p1Health.graphics.command.w = $rootScope.match.p1.character.health;
+            p2Health.graphics.command.w = $rootScope.match.p2.character.health;
+            $scope.$apply();
+        });
+        
+        $scope.gameResults = function() {
+            if (!$rootScope.match.inProgress) {
+                if ($rootScope.match.winner.name === $rootScope.player.name) {
+                    return 'You won!';
+                } else if ($rootScope.match.loser.name === $rootScope.player.name) {
+                    return 'You lost!';
+                }
+            }
+        };
+    }
     
     function tick() {
         // attack
@@ -151,11 +200,6 @@ angular.module('versus', [])
                 recoil += recoilRate;
             }
             allowedToAttack = false;
-        }
-        
-        // fade pendulum shadow
-        if (pendulumShadow.alpha > 0) {
-            pendulumShadow.alpha -= 0.01;
         }
         
         // set pendulum speed based on recoil
@@ -189,6 +233,11 @@ angular.module('versus', [])
         
         // center target
         target.x = stage.width / 2 - target.graphics.command.w / 2;
+        
+        // fade pendulum shadow
+        if (pendulumShadow.alpha > 0) {
+            pendulumShadow.alpha -= 0.01;
+        }
         
         stage.update();
     }
